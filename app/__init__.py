7cff1b9aeb8c37e1d3aeb385ad1045d438e2635b
Rajat Bajpai <rb.macboy@gmail.com>
2016-09-14 19:16:17 +0530

    Added some comments in models.py and __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,19 @@ from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 
 def create_app(config_name):
+    """
+        Create new Flask app object with specified
+        configurations
+
+        :param config_name:
+            configurations according to which Flask app
+            object to create (like development and production)
+    """
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,9 +1,23 @@
 from datetime import datetime
-
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
 
+"""
+    Class to represent single resource.
+
+    :attr id:
+        unique id of resource in the database
+
+    :attr path:
+        path where file stored in the disk
+
+    :attr password_hash:
+        hash value of password specified by user
+
+    :attr time_created (naive):
+        time when resource created
+"""
 class Item(db.Model):
     __tablename__ = 'items'
 
@@ -18,14 +32,36 @@ class Item(db.Model):
 
     @property
     def password(self):
+        """
+            Non readable property password
+        """
         raise AttributeError('password is not a readable property')
 
     @password.setter
     def password(self, password):
+        """
+            compute hash of password and store in field
+
+            :param password:
+                password which is specified by user
+        """
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
+        """
+            Verify password specified by user to the one present in
+            database.
+
+            :param password:
+                password specified by user to access resource
+        """
         return check_password_hash(self.password_hash, password)
 
     def set_path(self, path):
+        """
+            set the path where resource saved in disk
+
+            :param path:
+                path where resource is present
+        """
         self.path = path
